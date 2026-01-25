208ed6453b2c91121ba086d7c7ed3becb8349276
Moti Abe <motiabe8@gmail.com>
2026-01-25 10:07:56 +0300

    hackerrank submmition is added

---
+++ Phase-1/hakkerrank/arithimatic_operators.py
@@ -0,0 +1,11 @@
+#Task
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+#Solution
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
