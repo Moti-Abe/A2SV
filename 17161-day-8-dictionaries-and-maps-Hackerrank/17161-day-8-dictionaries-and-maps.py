6ee2e47e82c97955b170d76630527f01458dcb29
Moti Abe <motiabe8@gmail.com>
2026-02-04 10:00:27 +0300

    Added solution - LeetHub

--- 17161-day-8-dictionaries-and-maps-Hackerrank/17161-day-8-dictionaries-and-maps.py
+++ 17161-day-8-dictionaries-and-maps-Hackerrank/17161-day-8-dictionaries-and-maps.py
@@ -1,16 +1,19 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 n = int(input())
-mp = {}
+mp ={}
 
-for _ in range(n):
-    key, value = input().rsplit(" ", 1)
-    mp[key] = int(value)
-
+for i in range(n):
+    key, value = input().rsplit(" ",1)
+    value = int(value)
+    mp[key] = (value)
+    
 import sys
+
 for name in sys.stdin:
     name = name.strip()
     if name in mp:
         print(f"{name}={mp[name]}")
     else:
         print("Not found")
+        
