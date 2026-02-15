17840a07d399d53c02b8af7d2989f11715ed4a66
Moti-Abe <motiabe8@gmail.com>
2026-02-15 09:52:02 +0300

    some solved problems are added from the progress sheet

--- Phase-0/tempCodeRunnerFile.py
+++ Phase-0/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-    first_multiple_input = input().rstrip().split()
+# 30 min -> solved by my self
+# 45 min -> solved by my self + syntax reference
+# 60 min -> undersood but couldnt solve it by my self
+# 90 min -> niether understood nor solved by my selfff
+
+# some solved problems are added from the progress sheet
