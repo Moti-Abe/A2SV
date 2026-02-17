42efd67bd6ac610956b75fe3038249b0c5b7abda
Moti-Abe <motiabe8@gmail.com>
2026-02-17 08:45:09 +0300

    some solved problems are added from the progress sheet

---
+++ Phase-0/main.py
@@ -0,0 +1,4 @@
+queries = [[0,2],[1,4]]
+
+for ball, color in queries:
+    print(ball,color)
--- Phase-0/tempCodeRunnerFile.py
+++ Phase-0/tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
-# 30 min -> solved by my self
-# 45 min -> solved by my self + syntax reference
-# 60 min -> undersood but couldnt solve it by my self
-# 90 min -> niether understood nor solved by my selfff
+# 0 - 30 min -> solved by my self
+# 30 - 60 min -> solved by my self + syntax reference
+# 60 - 90 min -> undersood but couldnt solve it by my self
+# > 90 min -> AZA
 
 # some solved problems are added from the progress sheet
