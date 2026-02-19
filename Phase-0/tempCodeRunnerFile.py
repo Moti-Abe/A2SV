2d23a97cdf8593ad41bdfbdbdab84b85d6712dcb
Moti-Abe <motiabe8@gmail.com>
2026-02-19 22:22:19 +0300

    some solvrd problems are added

--- Phase-0/tempCodeRunnerFile.py
+++ Phase-0/tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
 # 0 - 30 min -> solved by my self
 # 30 - 60 min -> solved by my self + syntax reference
 # 60 - 90 min -> undersood but couldnt solve it by my self
-# > 90 min -> AZA
+# 90 - 120 min -> AZA
 
 # some solved problems are added from the progress sheet
