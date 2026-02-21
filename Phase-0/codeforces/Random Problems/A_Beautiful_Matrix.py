4ca2b6929a3f6918133df1d045dc857bec5cab1a
Moti-Abe <motiabe8@gmail.com>
2026-02-21 10:53:03 +0300

    #

---
+++ Phase-0/codeforces/Random Problems/A_Beautiful_Matrix.py
@@ -0,0 +1,13 @@
+matrix = []
+for _ in range(5):
+     row = list(map(int, input().strip().split()))
+     matrix.append(row)
+
+
+
+for i in range(5):
+     for j in range(5):
+          if matrix[i][j] == 1:
+               moves = abs(2 - i) + abs(2 - j)
+               print(moves)
+               break
