be4dc162eb33e96274c4a4a69fee3bc5c735d1da
Moti-Abe <motiabe8@gmail.com>
2026-02-27 09:53:38 +0300

    # some solved problems are added from the progress sheet

--- phase-1/leetcode/sort-colors_trial1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        for i in range (len(nums)):
-            for j in range(len(nums)-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-        
-        
