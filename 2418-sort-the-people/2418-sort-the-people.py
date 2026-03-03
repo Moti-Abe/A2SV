4a4e03cc263dcd1d3eba315b477b268f586c98a1
Moti Abe <motiabe8@gmail.com>
2026-03-03 10:27:29 +0300

    Time: 591 ms (13.36%), Space: 19.7 MB (49.25%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,21 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         mp = {}
-        output = []
         n = len(names)
         for i in range (n):
             mp[heights[i]] = names[i]
         
+        # Selection Sort
+        items = list(mp.items())
+        for i in range (n-1,-1,-1):
+            minpos = i
+            for j in range (i-1,-1,-1):
+                if items[j][0] < items[minpos][0]:
+                    minpos = j
+            items[i],items[minpos] = items[minpos], items[i]
+        sorted_mp = dict(items)
         
-        sorted_mp = dict(sorted(mp.items(),reverse=True))
-        
+        output = []
         for height,name in sorted_mp.items():
             output.append(name)
         
