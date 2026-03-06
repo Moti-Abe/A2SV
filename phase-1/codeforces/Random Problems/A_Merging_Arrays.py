1088267dca17cb6218b280c1e25b982456584625
Moti-Abe <motiabe8@gmail.com>
2026-03-06 14:19:38 +0300

    Codeforces merge two arrays problem using two ptr

--- phase-1/codeforces/Random Problems/A_Merging_Arrays.py
+++ phase-1/codeforces/Random Problems/A_Merging_Arrays.py
@@ -4,12 +4,22 @@ arr2 = list(map(int, input().split()))
 res = []
 left , right = 0 , 0
 
-while left < len(arr1) and right < len(arr2):
+while left < n and right < m:
     if arr1[left] < arr2[right]:
         res.append(arr1[left])
         left += 1
     else:
         res.append(arr2[right])
         right += 1
+
+while left == n and right <  m:
+    res.append(arr2[right])
+    right += 1
+
+while right == m and left < n:
+    res.append(arr1[left])
+    left += 1
+
+print (*res)
         
 
