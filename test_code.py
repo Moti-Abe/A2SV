74b678fec43293d45a8f9fa47c4271920be3d479
Moti-Abe <motiabe8@gmail.com>
2026-03-29 15:30:08 +0300

    Some solved problems added

--- test_code.py
+++ test_code.py
@@ -1,3 +1,9 @@
-s = "Hello, World!"
-if "lol" in s:
-    print(True)
+class person:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        print("Hello" , self.name)
+
+p1 = person("Moti")
+p1.greet()
