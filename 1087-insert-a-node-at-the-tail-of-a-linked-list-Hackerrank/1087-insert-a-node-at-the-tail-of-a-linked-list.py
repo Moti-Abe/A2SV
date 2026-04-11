85a7586e4f6236c4600c96d6e7f1df144759f25c
Moti Abe <motiabe8@gmail.com>
2026-04-11 13:55:56 +0300

    Added solution - LeetHub

---
+++ 1087-insert-a-node-at-the-tail-of-a-linked-list-Hackerrank/1087-insert-a-node-at-the-tail-of-a-linked-list.py
@@ -0,0 +1,24 @@
+
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    new_node = SinglyLinkedListNode(data)
+    
+    if head is None:
+        return new_node
+        
+    current = head
+    while current.next:
+        current = current.next
+    current.next = new_node
+    return head
+    
