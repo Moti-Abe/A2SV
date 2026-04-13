30907696ff057907ac58a592a538838046875488
Moti Abe <motiabe8@gmail.com>
2026-04-13 14:19:15 +0300

    Time: 20 ms (71.41%), Space: 21 MB (80.68%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -4,15 +4,14 @@ class Solution:
         n = len(nums)
         nge = [-1]*n
 
-        for i in range (n-1,-1,-1):
-            while stack and stack[-1] < nums[i]:
+        for i in range (2*n-1,-1,-1):
+            ind = i%n
+            while stack and stack[-1] <= nums[ind]:
                 stack.pop()
-            if stack:
-                nge[i] = stack[-1]
-            stack.append(nums[i])
+            if i < n:
+                if stack:
+                    nge[i] = stack[-1]
         
-        for i in range (n):
-            if nums[i] > nums[-1]:
-                nge[-1] = nums[i]
-                break
+            stack.append(nums[ind])
+           
         return nge
