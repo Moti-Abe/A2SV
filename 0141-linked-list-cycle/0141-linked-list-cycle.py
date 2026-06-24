dd088fea8a34cbf44292c9d7c14603d9d38795ff
Moti Abe <motiabe8@gmail.com>
2026-06-24 13:42:40 +0300

    Time: 59 ms (24.56%), Space: 22.5 MB (85.58%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        curr = head 
+        count = 0
+        while curr:
+            curr = curr.next
+            count += 1
+            if count > 10000:
+                return True
+        return False
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
